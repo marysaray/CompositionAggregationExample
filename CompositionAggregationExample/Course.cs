1a958b94c287e5580b01a482c678d8d146e6f474
Mary Saray <msaray.ms@outlook.com>
2021-07-20 21:17:38 -0700

    Created course class

---
+++ CompositionAggregationExample/Course.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositionAggregationExample
+{
+    /// <summary>
+    /// Represents one course that is related to a specific program.
+    /// </summary>
+    class Course
+    {
+        /// <summary>
+        /// The course code and title of the course.
+        /// Example: CPW 212: Advanced .NET Programming
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The official course catalog description.
+        /// </summary>
+        public string Descrition { get; set; }
+
+        /// <summary>
+        /// The number of college credits awarded for 
+        /// passing the course.
+        /// </summary>
+        public byte Credits { get; set; }
+
+        /// <summary>
+        /// The instructor that teaches the course.
+        /// </summary>
+        public Instructor CourseInstructor { get; set; }
+
+        /// <summary>
+        /// All students who are currently enrolled in the class.
+        /// </summary>
+        public List<Student> Roster { get; set; }
+    }
+}
